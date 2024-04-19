b3fe60c9f3f04d94b27efc89df44910414e71bf5
quangnd2203 <quangnd.nta@gmail.com>
2024-04-20 00:27:00 +0700

    no message

--- web/scripts/flutter_wallet_connect.js
+++ web/scripts/flutter_wallet_connect.js
@@ -1,44 +1,44 @@
-const { Web3AuthNoModal } = require('@web3auth/no-modal');
-const { OpenloginAdapter } = require("@web3auth/openlogin-adapter");
-const { EthereumPrivateKeyProvider } = require('@web3auth/ethereum-provider');
-const { CHAIN_NAMESPACES, WEB3AUTH_NETWORK, WALLET_ADAPTERS } = require('@web3auth/base');
-const { WalletConnectV2Adapter } = require('@web3auth/wallet-connect-v2-adapter');
+// Import các modules cần thiết từ thư viện '@web3modal/ethers'
+const { createWeb3Modal, defaultConfig } = require('@web3modal/ethers');
 
-const chainConfig = {
-  chainId: "0x1", // Please use 0x1 for Mainnet
-  chainNamespace: CHAIN_NAMESPACES.EIP155,
-  rpcTarget: "https://rpc.ankr.com/eth",
-  displayName: "Ethereum Mainnet",
-  blockExplorerUrl: "https://etherscan.io/",
-  ticker: "ETH",
-  tickerName: "Ethereum",
-  logo: "https://images.toruswallet.io/eth.svg",
+// 1. Lấy projectId tại https://cloud.walletconnect.com
+const projectId = 'c36bf582b97350dd8130834ceb358c39';
+
+// 2. Định nghĩa chain cho mainnet
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
 };
 
-const ethereumPrivateKeyProvider = new EthereumPrivateKeyProvider({
-  config: { chainConfig },
+// 3. Tạo đối tượng metadata cho ứng dụng của bạn
+const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+};
+
+// 4. Tạo config cho Ethers
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // mặc định là true
 });
 
-const web3auth = new Web3AuthNoModal({
-  clientId: 'BHoJV3aYHRUoGYd9p9Mxv-TgP42x4tbA6sTgbkIjINzBLXZL4UzHT0LKmxslMxDAGaYDm6vLO_oZF6yvD_Lf-U0',
-  web3AuthNetwork: WEB3AUTH_NETWORK.MAINNET,
-  chainConfig: chainConfig,
-  privateKeyProvider: ethereumPrivateKeyProvider,
-})
-
-const wc2Adapter = new WalletConnectV2Adapter({
-  adapterSettings: {
-    walletConnectInitOptions: {
-      projectId: "c36bf582b97350dd8130834ceb358c39",
-    }
-  },
-  chainConfig: chainConfig,
-})
-
-web3auth.configureAdapter(wc2Adapter);
-
-web3auth.init().then(async () => {
-  await web3auth.connectTo(WALLET_ADAPTERS.WALLET_CONNECT_V2)
+// 5. Tạo một instance của Web3Modal
+const modal = createWeb3Modal({
+  ethersConfig,
+  chains: [mainnet],
+  projectId,
+  enableAnalytics: true, // Optional - mặc định là cấu hình từ Cloud của bạn
+  enableOnramp: true // Optional - mặc định là false
 });
 
-window.web3auth = web3auth
+window.walletConnect = modal;
+
+window.walletConnect.open();
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -4,7 +4,6 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = {
   entry: {
     flutter_wallet_connect: './scripts/flutter_wallet_connect.js',
-    wallect_connect_etherum: './scripts/wallect_connect_etherum.js'
   },
   plugins: [
     new NodePolyfillPlugin()
